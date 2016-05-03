7dfaa037be669ba0eb3d8191cbbdfbc3b94630c5
MKormann <matt.kormann@gmail.com>
2016-05-03 11:19:40 -0400

    Added fragment switching in MainActivity

--- app/src/main/java/com/mattkormann/tournamentmanager/MainActivity.java
+++ app/src/main/java/com/mattkormann/tournamentmanager/MainActivity.java
@@ -1,32 +1,65 @@
     package com.mattkormann.tournamentmanager;
 
     import android.os.Bundle;
-    import android.support.design.widget.FloatingActionButton;
-    import android.support.design.widget.Snackbar;
+    import android.support.v4.app.Fragment;
     import android.support.v4.app.FragmentActivity;
-    import android.support.v7.app.AppCompatActivity;
-    import android.support.v7.widget.Toolbar;
-    import android.view.View;
+    import android.support.v4.app.FragmentTransaction;
 
-    public class MainActivity extends FragmentActivity {
+    public class MainActivity extends FragmentActivity
+            implements MainMenuFragment.onMenuButtonPressedListener {
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-			
+
+            //Check if there is a fragment container
             if (findViewById(R.id.fragment_container) != null) {
 
+                //Check if there is already a fragment displayed
                 if (savedInstanceState != null) {
                     return;
                 }
 
-                MainActivityFragment mainFragment = new MainActivityFragment();
+                //Create and display fragment for the main menu
+                MainMenuFragment mainFragment = new MainMenuFragment();
                 mainFragment.setArguments(getIntent().getExtras());
                 getSupportFragmentManager().beginTransaction()
                         .add(R.id.fragment_container, mainFragment).commit();
             }
 
+        }
+
+        //Replace fragment in fragment_container with id of corresponding button passed.
+        //Implementation of interface from MainMenuFragment.java
+        public void onMainMenuButtonPressed(int buttonID) {
+
+            Fragment fragment = new Fragment();
+
+            switch (buttonID) {
+                case (R.id.button_start_tournament):
+                    //fragment = new StartTournamentFragment();
+                    break;
+                case (R.id.button_create_tournament):
+                    //fragment = new CreateTournamentFragment();
+                    break;
+                case (R.id.button_participants):
+                    //fragment = new ParticipantsFragment();
+                    break;
+                case (R.id.button_teams):
+                    //fragment = new TeamsFragment();
+                    break;
+                case (R.id.button_history):
+                    //fragment = new HistoryFragment();
+                    break;
+            }
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.fragment_container, fragment);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
         }
 
     }
---
+++ app/src/main/java/com/mattkormann/tournamentmanager/MainMenuFragment.java
@@ -0,0 +1,66 @@
+package com.mattkormann.tournamentmanager;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainMenuFragment extends Fragment implements
+        View.OnClickListener {
+
+    onMenuButtonPressedListener mCallback;
+
+    public MainMenuFragment() {
+    }
+
+    //Interface to be implemented by containing activity.
+    public interface onMenuButtonPressedListener {
+        public void onMainMenuButtonPressed(int buttonID);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Listeners for buttons
+        Button startButton = (Button)view.findViewById(R.id.button_start_tournament);
+        startButton.setOnClickListener(this);
+        Button createButton = (Button)view.findViewById(R.id.button_create_tournament);
+        createButton.setOnClickListener(this);
+        Button participantsButton = (Button)view.findViewById(R.id.button_participants);
+        participantsButton.setOnClickListener(this);
+        Button teamsButton = (Button)view.findViewById(R.id.button_teams);
+        teamsButton.setOnClickListener(this);
+        Button historyButton = (Button)view.findViewById(R.id.button_history);
+        historyButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    //Checks interface is implemented in the activity.
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+
+        try {
+            mCallback = (onMenuButtonPressedListener)context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    " must implement onMenuButtonPressedListener");
+        }
+        MainMenuFragment fg = new MainMenuFragment();
+    }
+
+    @Override
+    public void onClick(View view) {
+        mCallback.onMainMenuButtonPressed(view.getId());
+    }
+}
