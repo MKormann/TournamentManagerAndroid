b36d98cb7a20fc9a5b65bbddb7747e9b99065d6e
MKormann <matt.kormann@gmail.com>
2016-05-02 20:36:10 -0400

    Edits to main fragment classes

---
+++ app/src/test/java/com/mattkormann/tournamentmanager/sql/DatabaseHelperTest.java
@@ -0,0 +1,19 @@
+package com.mattkormann.tournamentmanager.sql;
+
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import org.junit.Test;
+
+/**
+ * Created by Matt on 5/2/2016.
+ */
+public class DatabaseHelperTest extends AndroidTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+}
